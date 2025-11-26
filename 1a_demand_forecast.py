375e982d30d33f9c26a6c8d633e10bb366a23f5a
tgod <tgod@student.tudelft.nl>
2025-11-26 11:23:03 +0100

    nogsteeds data lezen

--- 1a_demand_forecast.py
+++ 1a_demand_forecast.py
@@ -4,22 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path_pop = 'Problem 1 - Data/pop.xlsx'
-path_demand = 'Problem 1 - Data/DemandGroup17.xlsx'
+path_demand = 'Problem 1 - Data/demand_per_week.xlsx'
+path_airports = 'Problem 1 - Data/airport_data.xlsx'
 path_aircraft_data = 'Problem 1 - Data/AircraftData.xlsx'
 
 earth_radius = 6371  # in kilometers
 
 def load_data():
-    pop_data = pd.read_excel(path_pop, header=2).dropna(how='all', axis=1).to_numpy()
-    demand_data = pd.read_excel(path_demand).dropna(how='all', axis=0).to_numpy()
-    aircraft_data = pd.read_excel(path_aircraft_data).to_numpy()
+    pop_GDP_data = pd.read_excel(path_pop, header=2, index_col=0).dropna(how='all', axis=1)
 
-    airport_data = 
-
-    return pop_data, demand_data, aircraft_data
-
-print(load_data()[1])
+    demand_data = pd.read_excel(path_demand, index_col=0)
+    airport_data = pd.read_excel(path_airports)
 
 
-#def convert_distance(path_demand, radius=earth_radius):
+    return pop_GDP_data, demand_data, airport_data
+
+print(load_data()[0])
+
+
     
